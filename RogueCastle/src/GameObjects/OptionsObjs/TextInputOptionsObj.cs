90ac22a8a356f1ac94bf9c30e5cd446dd26c87d3
Zach Parks <zach@alliware.com>
2024-10-13 15:45:20 -0500

    Minor cleanup of text options

--- RogueCastle/src/GameObjects/OptionsObjs/TextInputOptionsObj.cs
+++ RogueCastle/src/GameObjects/OptionsObjs/TextInputOptionsObj.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using DS2DEngine;
 using Microsoft.Xna.Framework;
@@ -8,22 +7,19 @@ namespace RogueCastle.GameObjects.OptionsObjs;
 
 public sealed class TextInputOptionsObj : RandomizerOptionsObj
 {
+    private readonly bool    _hidden;
+    private readonly string  _placeholder;
+    private          string  _currentValue = "";
+    private          int     _cursorIndex;
+    private          TextObj _valueText;
 
-    private bool             _ready;
-    private bool             _hidden;
-    private string           _placeholder;
-    private string           _currentValue = "";
-
-    private TextObj          _valueText;
-    private int              _cursorIndex;
-    
     public TextInputOptionsObj(string title, string placeholder = "", bool hidden = false) : base(title)
     {
         _placeholder = placeholder;
         _hidden = hidden;
 
         TitleText.Align = Types.TextAlign.Right;
-        
+
         // Toggle text highlighting.
         _valueText = TitleText.Clone() as TextObj;
         _valueText!.X = OPTIONS_TEXT_OFFSET;
@@ -49,16 +45,15 @@ public sealed class TextInputOptionsObj : RandomizerOptionsObj
             {
                 _valueText.TextureColor = Color.Yellow;
                 _cursorIndex = _currentValue.Length;
-                _valueText.Text = _hidden 
-                    ? string.Concat(Enumerable.Repeat("*", _currentValue.Length)) 
+                _valueText.Text = _hidden
+                    ? string.Concat(Enumerable.Repeat("*", _currentValue.Length))
                     : _currentValue;
-                
+
                 TextInputEXT.TextInput += InterceptKey;
                 TextInputEXT.StartTextInput();
                 return;
             }
-            
-            _ready = false;
+
             _valueText.TextureColor = _currentValue.Length > 0 ? Color.White : Color.Gray;
 
             if (_hidden)
@@ -71,7 +66,7 @@ public sealed class TextInputOptionsObj : RandomizerOptionsObj
             }
         }
     }
-    
+
     public override void HandleInput()
     {
         // Do not leave early if we're still typing.
@@ -80,7 +75,7 @@ public sealed class TextInputOptionsObj : RandomizerOptionsObj
             SoundManager.PlaySound("Options_Menu_Deselect");
             IsActive = false;
         }
-        
+
         if (IsActive)
         {
             if (_hidden)
@@ -107,7 +102,7 @@ public sealed class TextInputOptionsObj : RandomizerOptionsObj
             _cursorIndex++;
             return;
         }
-        
+
         // Control character.
         switch (chr)
         {
@@ -122,7 +117,7 @@ public sealed class TextInputOptionsObj : RandomizerOptionsObj
 
                 break;
             }
-            
+
             // Enter/Return (to confirm input)
             case '\n':
             case '\r':
@@ -131,14 +126,14 @@ public sealed class TextInputOptionsObj : RandomizerOptionsObj
                 break;
         }
     }
-    
+
     public override void Dispose()
     {
         if (IsDisposed)
         {
             return;
         }
-        
+
         _valueText?.Dispose();
         _valueText = null;
         base.Dispose();
